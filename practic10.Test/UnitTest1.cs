2bb9e409b4235fee8493478712d55ebe0e9644c3
Илья <@DESKTOP-5J69AGE>
2026-03-03 10:06:55 +0500

    Добавьте файлы проекта.

---
+++ practic10.Test/UnitTest1.cs
@@ -0,0 +1,138 @@
+﻿using practic10;
+
+namespace practic10.Test
+{
+    public class CheckerBoardPositionTes()
+    {
+        // Constructor
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(8, 8)]
+        [InlineData(4, 7)]
+
+        public void Constructor_ValidValues_CreateObject(byte x, byte y)
+        {
+            var obj = new CheckerBoardPosition(x, y);
+
+            Assert.Equal(x, obj.X);
+            Assert.Equal(y, obj.Y);
+
+        }
+
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(1, 0)]
+        [InlineData(100, 2)]
+        [InlineData(2, 100)]
+        public void Constructor_InValidValues_ArgumentOutOfRangeException(byte x, byte y)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>  new CheckerBoardPosition(x, y));
+        }
+
+
+        // XLetter
+        [Theory]
+        [InlineData(1, 'A')]
+        [InlineData(2, 'B')]
+        [InlineData(3, 'C')]
+        [InlineData(4, 'D')]
+        [InlineData(5, 'E')]
+        [InlineData(6, 'F')]
+        [InlineData(7, 'G')]
+        [InlineData(8, 'H')]
+        public void XLetter_ValidValues_CorrectLetter(byte x, char letter) 
+        {
+            var obj = new CheckerBoardPosition(x, 1);
+
+            Assert.Equal(letter, obj.XLetter);
+        }
+
+
+        // ToString()
+        [Theory]
+        [InlineData(1, 1, "A1" )]
+        [InlineData(8, 8, "H8" )]
+        [InlineData(4, 6, "D6" )]
+        public void ToString_ValidValues_CorrectString(byte x, byte y, string s)
+        {
+            var obj = new CheckerBoardPosition(x, y);
+
+            Assert.Equal(s, obj.ToString());
+        }
+
+        // Parse()
+        [Theory]
+        [InlineData("A1", 1, 1)]
+        [InlineData("H8", 8, 8)]
+        [InlineData("D4", 4, 4)]
+        [InlineData("C7", 3, 7)]
+        public void Parse_ValidValues_CorrectObject(string s, byte x, byte y)
+        {
+            var obj = CheckerBoardPosition.Parse(s, null);
+
+            Assert.Equal(x, obj.X);
+            Assert.Equal(y, obj.Y);
+        }
+
+        [Theory]
+        [InlineData("fadafd")]
+        [InlineData("11")]
+        [InlineData("O0")]
+        [InlineData("1412")]
+        public void Parse_InValidValues_ThrowFormatException(string s)
+        {
+            Assert.Throws<FormatException>(() => CheckerBoardPosition.Parse(s, null));
+        }
+
+        [Fact]
+        public void Parse_NullString_ThrowsFormatException()
+        {
+            Assert.Throws<FormatException>(() => CheckerBoardPosition.Parse(null, null));
+        }
+
+        // TryParse()
+        [Theory]
+        [InlineData("A1", 1, 1)]
+        [InlineData("H8", 8, 8)]
+        [InlineData("D4", 4, 4)]
+        [InlineData("C7", 3, 7)]
+        public void TryParse_ValidValues_True(string s, byte x, byte y)
+        {
+            var res = CheckerBoardPosition.TryParse(s, null, out var obj);
+
+            Assert.True(res);
+            Assert.Equal(x, obj.X);
+            Assert.Equal(y, obj.Y);
+        }
+
+        [Theory]
+        [InlineData("B0")]
+        [InlineData("J6")]
+        [InlineData("asd")]
+        [InlineData("pp")]
+        [InlineData("A4 ")]
+        [InlineData("")]
+        [InlineData("A")]     
+        [InlineData("A123")]  
+        [InlineData("1A")]   
+        [InlineData("AA")]    
+        [InlineData("11")]
+        public void TryParse_InvalidValues_False(string s)
+        {
+            var res = CheckerBoardPosition.TryParse(s, null, out var obj);
+
+            Assert.False(res);
+            Assert.Null(obj);
+        }
+
+        [Fact]
+        public void TryParse_NullString_False() 
+        {
+            var res = CheckerBoardPosition.TryParse(null, null, out var obj);
+
+            Assert.False(res);
+            Assert.Null(obj);
+        }
+
+    }
+}
---
+++ practic10/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace practic10
+{
+    public class CheckerBoardPosition(byte x, byte y) : IParsable<CheckerBoardPosition>
+    {
+
+        [AllowedValues(1, 2, 3, 4, 5, 6, 7, 8)]
+        public byte X { get; } = x is > 0 and <= 8 ? x : throw new ArgumentOutOfRangeException(nameof(x));
+
+        [AllowedValues(1, 2, 3, 4, 5, 6, 7, 8)]
+        public byte Y { get; } = y is > 0 and <= 8 ? y : throw new ArgumentOutOfRangeException(nameof(y));
+
+        private const char LetterOffset = '@'; // 'A' - 1
+
+        public char XLetter => (char)(LetterOffset + X);
+
+        public override string ToString() => $"{XLetter}{Y}";
+
+        public static CheckerBoardPosition Parse(string s, IFormatProvider? provider)
+            => TryParse(s, provider, out var result)
+                ? result
+                : throw new FormatException($"Invalid {nameof(CheckerBoardPosition)}: {s}");
+
+        public static bool TryParse(
+            [NotNullWhen(true)] string? s,
+            IFormatProvider? provider,
+            [NotNullWhen(true)] out CheckerBoardPosition? result)
+        {
+            if (s is [var x and >= 'A' and <= 'H', var y and >= '1' and <= '8'])
+            {
+                result = new CheckerBoardPosition((byte)(x - LetterOffset), byte.Parse([y]));
+                return true;
+            }
+
+            result = null;
+            return false;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+}
